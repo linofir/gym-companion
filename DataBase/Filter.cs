b69471734f2e633ac7c8394fbc0f9aace7a18229
linofir <firmanoster@gmail.com>
2023-11-05 09:43:50 -0300

    basic filters class

--- DataBase/Filter.cs
+++ DataBase/Filter.cs
@@ -1,8 +1,78 @@
 using GymCompanion.Modules;
 namespace GymCompanion.DataBase;
 
+public enum Musculos
+{
+    Null = 0,
+    Abdominals,
+    Hamstrings,
+    Adductors,
+    Quadriceps,
+    Biceps,
+    Shoulders,
+    Chest,
+    MiddleBack,
+    Calves,
+    Glutes,
+    LowerBack,
+    Lats,
+    Triceps,
+    Traps,
+    Forearms,
+    Neck,
+    Abductors
+}
+
+public enum Filtros
+{
+    Id,
+    Nome,
+    GrupoMuscular,
+    Nivel,
+    Mecanica,
+    Categoria,
+    Tipo,
+    Equipamento
+}
 internal class Filter
 {
+    public List<string> MuscString = new()
+    {
+        "",
+        "Abdominals",
+        "Hamstrings",
+        "Adductors",
+        "Quadriceps",
+        "Biceps",
+        "Shoulders",
+        "Chest",
+        "MiddleBack",
+        "Calves",
+        "Glutes",
+        "LowerBack",
+        "Lats",
+        "Triceps",
+        "Traps",
+        "Forearms",
+        "Neck",
+        "Abductors",
+    };
+    public static void FilterBy(Exercicios listaConsulta, string nome)
+    {
+        var exerciciosEncontrados = listaConsulta.listaExercicios.Where(ex => ex.Nome!.Contains(nome)).ToList();
+        if(exerciciosEncontrados != null)
+        {
+            Console.WriteLine($"Foi encontrado {exerciciosEncontrados.Count} exercício(s): \n");
+            int counter = 0;
+            foreach (var exercicio in exerciciosEncontrados)
+            {
+                counter += 1;
+                Console.WriteLine($"Exercício {counter}: \n{exercicio.Descricao}");
+            }
+        }else Console.WriteLine("Nenhum Exercício encontrado");
+        
+    }
+
     //nome, id, grupo muscular, nivel, categoria
     public static void FilterByName(Exercicios listaConsulta, string nome)
     {
@@ -36,11 +106,13 @@ internal class Filter
         
     }
 
-    public static void FilterByNivel(Exercicios listaConsulta, string nivel)
+    public static Exercicios FilterByNivel(Exercicios listaConsulta, string nivel)
     {
         var exerciciosEncontrados = listaConsulta.listaExercicios.Where(ex => ex.Nivel!.Contains(nivel)).ToList();
+        Exercicios listaFiltrada = new("listaFiltrada");
         if(exerciciosEncontrados != null)
         {
+            listaFiltrada.listaExercicios = exerciciosEncontrados!;
             Console.WriteLine($"Foi encontrado {exerciciosEncontrados.Count} exercício(s): \n");
             int counter = 0;
             foreach (var exercicio in exerciciosEncontrados)
@@ -50,14 +122,17 @@ internal class Filter
                 Console.WriteLine($"Exercício {counter}: \n{exercicio.Descricao}");
             }
         }else Console.WriteLine("Nenhum Exercício encontrado");
+        return listaFiltrada;
         
     }
 
-    public static void FilterByCategoria(Exercicios listaConsulta, string categoria)
+    public static Exercicios FilterByCategoria(Exercicios listaConsulta, string categoria)
     {
         var exerciciosEncontrados = listaConsulta.listaExercicios.Where(ex => ex.Categoria!.Contains(categoria)).ToList();
+        Exercicios listaFiltrada = new("listaFiltrada");
         if(exerciciosEncontrados != null)
         {
+            listaFiltrada.listaExercicios = exerciciosEncontrados!;
             Console.WriteLine($"Foi encontrado {exerciciosEncontrados.Count} exercício(s): \n");
             int counter = 0;
             foreach (var exercicio in exerciciosEncontrados)
@@ -67,13 +142,37 @@ internal class Filter
                 Console.WriteLine($"Exercício {counter}: \n{exercicio.Descricao}");
             }
         }else Console.WriteLine("Nenhum Exercício encontrado");
+        return listaFiltrada;
     }
 
-    public static void FilterByGrupoMuscular(Exercicios listaConsulta, string grupoMuscular)
+    public static Exercicios FilterByGrupoMuscular(Exercicios listaConsulta, Musculos musculo)
     {
+        string grupoMuscular = musculo.ToString();
         var exerciciosEncontrados = listaConsulta.listaExercicios.Where(ex => ex.GrupoMuscular!.Contains(grupoMuscular)).ToList();
+        Exercicios listaFiltrada = new("listaFiltrada");
         if(exerciciosEncontrados != null)
         {
+            listaFiltrada.listaExercicios = exerciciosEncontrados!;
+            Console.WriteLine($"Foi encontrado {exerciciosEncontrados.Count} exercício(s): \n");
+            int counter = 0;
+            foreach (var exercicio in exerciciosEncontrados)
+            {
+                counter += 1;
+
+                Console.WriteLine($"Exercício {counter}: \n{exercicio.Descricao}");
+                Console.WriteLine(musculo.ToString().GetType());
+            }
+        }else Console.WriteLine("Nenhum Exercício encontrado");
+        return listaFiltrada!;
+    }
+
+    public static Exercicios FilterByGrupoEquip(Exercicios listaConsulta, string equipamento)
+    {
+        var exerciciosEncontrados = listaConsulta.listaExercicios.Where(ex => ex.Equipamento! == null || ex.Equipamento!.Contains(equipamento)).ToList();
+        Exercicios listaFiltrada = new("listaFiltrada");
+        if(exerciciosEncontrados != null)
+        {
+            listaFiltrada.listaExercicios = exerciciosEncontrados!;
             Console.WriteLine($"Foi encontrado {exerciciosEncontrados.Count} exercício(s): \n");
             int counter = 0;
             foreach (var exercicio in exerciciosEncontrados)
@@ -83,6 +182,7 @@ internal class Filter
                 Console.WriteLine($"Exercício {counter}: \n{exercicio.Descricao}");
             }
         }else Console.WriteLine("Nenhum Exercício encontrado");
+        return listaFiltrada!;
     }
 
     public static void ExibirGruposMusculares(Exercicios listaConsulta)
@@ -95,8 +195,8 @@ internal class Filter
             foreach (var musculo in musculosEncontrados)
             {
                 counter += 1;
-
-                Console.WriteLine($"-{musculo}");
+                string musculoUpper = char.ToUpper(musculo[0]) + musculo.Substring(1);
+                Console.WriteLine($"{musculoUpper},");
             }
         }else Console.WriteLine("Nenhum Exercício encontrado");
     }
--- Program.cs
+++ Program.cs
@@ -11,12 +11,28 @@ CreateDB.ConsultDB(listaData);
 //listaData.ExibirExercicios();
 // Filter.FilterByName(listaData, "Zercher Squats");
 // Filter.FilterByID(listaData, "9db21b40-e86f-43ea-ab34-e8430bb0a637");
-//Filter.FilterByNivel(listaData, "intermediate");
 // Filter.FilterByCategoria(listaData, "strength");
-// Filter.FilterByGrupoMuscular(listaData, "biceps");
+// Filter.ExibirGruposMusculares(listaData);
+
+var listaFiltradaMusc = Filter.FilterByGrupoMuscular(listaData, Musculos.Biceps);
+
+//var listaFiltradaCategoria = Filter.FilterByCategoria(listaFiltradaMusc, "strength");
+
+//var listaFiltradaEquip = Filter.FilterByGrupoEquip(listaFiltradaMusc, "machine");
+
+//var listaFiltradaNivel = Filter.FilterByNivel(listaFiltradaEquip, "beginner");
+
+//Filter.FilterByName(listaFiltradaMusc,"Hip");
 
 
-Filter.ExibirGruposMusculares(listaData);
+List<string> treinoTriceps = new(){"Standing One-Arm Dumbbell Triceps Extension", "Dip Machine"};
+List<string> treinoTricepsId = new(){"3551cc89-1417-4160-b89e-edf1fbac78d0", "ab7c67c4-2dff-4046-bc2c-15946af9fb20"};
+
+List<string> treinoPeito = new(){"Barbell Bench Press - Medium Grip", "Barbell Incline Bench Press - Medium Grip", "Straight-Arm Dumbbell Pullover", "Butterfly"}; 
+List<string> treinoPeitoId = new(){"766364a8-b555-4d21-9766-2f0a85a08e71", "4c25fa61-84a2-4f05-a07d-a70aaba61f0b", "1d807b2e-ab35-4aff-b5f2-048ed4140e35", "7a987c80-08b7-4051-8e3d-d9bf45cc7696"};
+
+List<string> treinoAbs = new(){"Sit-Up", "Plank", "Oblique Crunches - On The Floor","Knee/Hip Raise On Parallel Bars"};
+List<string> treinoAbsId = new(){"67487e95-3db4-4ab0-9126-51f9337d6786", "340a26b5-a644-4496-99ff-262f82d1b03f","272027ea-7f52-46be-b3ac-9eccfa5c8e4d", "9ef17b2a-b798-4881-af7a-4fac94e2f95a"};
 
 
 
